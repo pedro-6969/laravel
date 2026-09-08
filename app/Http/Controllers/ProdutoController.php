1786f74d9a4ebe45e512961276b56a7421c5e433
pedro-6969 <pedromiguel19712@gmail.com>
2026-09-08 14:48:02 -0300

    Paginate

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use App\Http\Requests\StoreProdutoRequest;
+use App\Http\Requests\UpdateProdutoRequest;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(int $id, string $nome)
+    {
+        $produtos = Produto::where('nome', 'like', '%'.$nome.'%')
+        ->paginate(2);
+        
+        return view('produtos', ['id'=>$id, 'nome'=>$nome]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProdutoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProdutoRequest $request, Produto $produto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Produto $produto)
+    {
+        //
+    }
+}
