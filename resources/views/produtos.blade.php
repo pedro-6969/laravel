a9b835c996dc5bd3c87369fa7bae7e4eb5563ad0
pedro-6969 <pedromiguel19712@gmail.com>
2026-09-08 14:50:05 -0300

    paginate

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,6 +1,3 @@
 <h1>Id: {{ $id }}</h1>
 <h1>Nome: {{ $nome }}</h1>
 
-<a href="{{ route('produtos', ['id'=>123, 'nome'=>'carro']) }}">Produto 1</a><br>
-<a href="{{ route('produtos', ['id'=>321, 'nome'=>'moto']) }}">Produto 1</a><br>
-<a href="{{ route('produtos', ['id'=>312, 'nome'=>'bicicleta']) }}">Produto 1</a><br>
